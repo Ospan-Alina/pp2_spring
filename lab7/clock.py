959ead844f6e221a2b267f26fa2875f0a42ee266
Ospan <al_ospan@kbtu.kz>
2024-04-07 00:13:02 +0500

    lab7

--- lab7/clock.py
+++ lab7/clock.py
@@ -1,40 +1,44 @@
-import pygame as pg 
-from datetime import datetime as dt
+import pygame 
+import datetime 
+ 
+ 
+pygame.init() 
+screen = pygame.display.set_mode((800,600)) 
+ 
+clock=pygame.time.Clock() 
+ 
+white=(255,255,255) 
+ 
+image = pygame.image.load("images/mainclock.png")
+imageL = pygame.image.load("images/leftarm.png")
+imageR = pygame.image.load("images/rightarm.png")
 
-# pygame initialization
-pg.init()
-screen = pg.display.set_mode((600, 450))
-window_title = pg.display.set_caption("mickey mouse clock")
-#icon = pg.display.set_icon(pg.image.load(r"clock_assets/icon.png"))
-clock = pg.time.Clock()
-
-# loading the images 
-bg_surf = pg.image.load("images/mainclock.png")
-leftarm_surf = pg.image.load("images/leftarm.png")
-rightarm_surf = pg.image.load("images/rightarm.png")
-bg_rect = bg_surf.get_rect(center = (300, 225))
-
-done = False
-
-while not done:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            done = True
-
-    current_time = dt.now().time()
-
-    seconds_angle = -(current_time.second * 6)
-    minutes_angle = -(current_time.minute * 6)
-
-    rotated_leftarm = pg.transform.rotate(leftarm_surf, seconds_angle)
-    rotated_rightarm = pg.transform.rotate(rightarm_surf, minutes_angle)
-
-    leftarm_rect = rotated_leftarm.get_rect(center = (340, 250))
-    rightarm_rect = rotated_rightarm.get_rect(center = (230,230))
-
-    screen.blit(bg_surf, bg_rect)
-    screen.blit(rotated_leftarm, leftarm_rect)
-    screen.blit(rotated_rightarm, rightarm_rect)
-
-    pg.display.flip()
-    clock.tick(60)
+ 
+done = False 
+ 
+while not done: 
+        for event in pygame.event.get(): 
+                if event.type == pygame.QUIT: 
+                        done = True 
+        screen.fill(white) 
+ 
+        screen.blit(image, (50, 30)) 
+ 
+        curr_date=datetime.datetime.now() 
+        curr_min=curr_date.minute 
+        curr_sec=curr_date.second 
+        minute_angle = (curr_min +4 / 60) * (-360)   
+        second_angle = (curr_sec/ 60) * (-360)   
+ 
+        rotated_Rhand=pygame.transform.rotate(imageR,minute_angle) 
+        Rhand_move=rotated_Rhand.get_rect(center=(400, 300)) 
+        screen.blit(rotated_Rhand, Rhand_move) 
+ 
+        rotated_Lhand = pygame.transform.rotate(imageL, second_angle) 
+        Lhand_move = rotated_Lhand.get_rect(center=(400, 300)) 
+        screen.blit(rotated_Lhand, Lhand_move) 
+ 
+        pygame.display.flip() 
+        clock.tick(60) 
+ 
+pygame.quit()
